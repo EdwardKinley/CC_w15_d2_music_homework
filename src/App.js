417ff150b04c8850738ae921ec966e7f51cf53d8
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-08-28 21:33:41 +0100

    adds spinning record

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ChartBox from './containers/ChartBox.js';
+import record from './record.jpeg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <img src={record} className="App-logo" alt="logo" />
           <h1 className="App-title">iTunes Top 20</h1>
         </header>
         <ChartBox />
